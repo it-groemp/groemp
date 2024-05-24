8b6b75a9a07a1f6b2ffe91363a810c7476a4fd80
Invest TRade Profit Now <sddmsinvesttradeprofitnow@gmail.com>
2024-05-24 14:26:30 +0530

    Updated details of database

--- database/migrations/2024_05_18_161612_create_admins_table.php
+++ database/migrations/2024_05_18_161612_create_admins_table.php
@@ -13,11 +13,11 @@ return new class extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
+            $table->string("pan",10)->nullable(false);
             $table->string("name",50)->nullable(false);
             $table->string("mobile",10)->nullable(false);
             $table->string("email",100)->nullable(false);
-            $table->string("company",10)->nullable(false)->unique();
-            $table->foreign("company")->references("pan")->on("companies");
+            $table->string("company",10)->nullable(true);
             $table->string("role",10)->nullable(false)->default("Employer");
             $table->timestamp("from_date")->useCurrent();
             $table->timestamp("to_date")->nullable(true);
