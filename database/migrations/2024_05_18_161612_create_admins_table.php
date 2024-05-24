e676bb3fcc266cda65dfaabb4bcf6093b3b96397
Invest TRade Profit Now <sddmsinvesttradeprofitnow@gmail.com>
2024-05-24 22:13:40 +0530

    removed pan for Admins and Employer

--- database/migrations/2024_05_18_161612_create_admins_table.php
+++ database/migrations/2024_05_18_161612_create_admins_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
-            $table->string("pan",10)->nullable(false);
             $table->string("name",50)->nullable(false);
             $table->string("mobile",10)->nullable(false);
             $table->string("email",100)->nullable(false);
