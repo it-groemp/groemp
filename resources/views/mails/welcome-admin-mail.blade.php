f607fde0562d1b52a356251438a0b6678e004fe8
Invest TRade Profit Now <sddmsinvesttradeprofitnow@gmail.com>
2024-06-12 11:54:32 +0530

    updated email address of support group

--- resources/views/mails/welcome-admin-mail.blade.php
+++ resources/views/mails/welcome-admin-mail.blade.php
@@ -2,7 +2,7 @@
     <h2>Hello {{$name}},</h2>
     <p>Welcome to {{config("app.name")}} Admin. Your passsword is updated for our Website. </p>
     <p>You can login to our Website by clicking the link<a href="{{config('app.url').'/admin/login'}}">{{config('app.name')}}</a></p>
-    <p>In case of any queries, you can contact us on <a href="mailto:{{config('app.contact')}}?subject=Query">{{config('app.contact')}}</a></p>
+    <p>In case of any queries, you can contact us on <a href="mailto:{{config('mail.from.address')}}?subject=Query">{{config('mail.from.address')}}</a></p>
     <h2>
         Thanks,<br>
         {{config('app.name')}} Team
