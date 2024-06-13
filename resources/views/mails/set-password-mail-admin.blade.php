281d85a710105b2eb122e3af511ec1bd0f4798e5
Invest TRade Profit Now <sddmsinvesttradeprofitnow@gmail.com>
2024-06-13 13:02:09 +0530

    set the email address correctly

--- resources/views/mails/set-password-mail-admin.blade.php
+++ resources/views/mails/set-password-mail-admin.blade.php
@@ -2,7 +2,7 @@
     <h2>Hello {{$name}},</h2>
     <p>Welcome to {{config("app.name")}} Admin. This login is for the Admin section of our Website <a href="{{config('app.url')}}">{{config('app.name')}}</a></p>
     <p>To set the password to your login, you can click on this link <a href="{{$link}}">Set Password</a></p>
-    <p>In case of any queries, you can contact us on <a href="mailto:{{config('mail.from.address')}}?subject=Query">{{config('mail.from.address')}}</a></p>
+    <p>In case of any queries, you can contact us on <a href="mailto:{{config('app.contact')}}?subject=Query">{{config('app.contact')}}</a></p>
     <h2>
         Thanks,<br>
         {{config('app.name')}} Team
