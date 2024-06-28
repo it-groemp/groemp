12ae3100c54e7b8b81a4657e0be4811105e35c34
Invest TRade Profit Now <sddmsinvesttradeprofitnow@gmail.com>
2024-06-28 12:26:41 +0530

    Text field details added and benefit amount updated

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cart;
+use App\Models\Employee;
+use App\Models\EmployeeBenefit;
 use App\Models\EmployeeAvailedBenefits;
 
 use Carbon\Carbon;
@@ -89,7 +91,37 @@ class CartController extends Controller
         }
         Session::put("benefit_amount",$benefit_amount);
         return redirect()->back()->with("success","Benefit is added to the cart");
+    }
 
+    public function saveCartText(Request $request){
+        $request->validate([
+            "description" => "required"
+        ]);
+
+        $employee_id = Session::get("employee");
+
+        $benefit_id = $request->benefit_id;
+        $category_id = $request->category_id;
+        $name = $request->name;
+        $description = $request->description;
+
+        $cartBenefit = Cart::where("benefit_id",$benefit_id)->first();
+        if($cartBenefit==null){
+            $cartBenefit = new Cart();
+            $cartBenefit->employee_id = $employee_id;
+            $cartBenefit->benefit_id = $benefit_id;
+            $cartBenefit->category_id = $category_id;
+            $cartBenefit->benefit_name = $name;
+            $cartBenefit->description = $description;
+            $cartBenefit->price = 0;
+            $cartBenefit->quantity = 0;
+            $cartBenefit->save();
+        }
+        else{
+            $cartBenefit->description = $description;
+            $cartBenefit->update();
+        }
+        return redirect()->back()->with("success","Benefit is added to the cart");
     }
 
     public function deleteFromCart($id){
@@ -110,19 +142,29 @@ class CartController extends Controller
 
     public function saveBenefits(){
         $employee_id = Session::get("employee");
+        $employee = Employee::find($employee_id);
         $cart = Cart::where("employee_id",$employee_id)->get();
+        $total = 0;
         foreach($cart as $benefit){
-            $employee_benefits = new EmployeeAvailedBenefits();
-            $employee_benefits->employee_id = $benefit->employee_id;
-            $employee_benefits->date_availed = Carbon::now();
-            $employee_benefits->benefit_id = $benefit->benefit_id;
-            $employee_benefits->category_id = $benefit->category_id;
-            $employee_benefits->price = $benefit->price;
-            $employee_benefits->quantity = $benefit->quantity;
-            $employee_benefits->description = $benefit->description;
-            $employee_benefits->save();
-            $benefit->delete();
+            $employee_availed_benefits = new EmployeeAvailedBenefits();
+            $employee_availed_benefits->employee_id = $benefit->employee_id;
+            $employee_availed_benefits->date_availed = Carbon::now();
+            $employee_availed_benefits->benefit_id = $benefit->benefit_id;
+            $employee_availed_benefits->category_id = $benefit->category_id;
+            $employee_availed_benefits->price = $benefit->price;
+            $employee_availed_benefits->quantity = $benefit->quantity;
+            $employee_availed_benefits->description = $benefit->description;
+            $total = $total + $benefit->price;
+            $employee_availed_benefits->save();
+            //$benefit->delete();
         }
+
+        $employee_benefits = EmployeeBenefit::where("pan_number",$employee->pan_number)->first();             
+        $balance = $employee_benefits->current_benefit - $total;
+        $employee_benefits->availed_benefit = $total;
+        $employee_benefits->current_benefit = $balance;
+        $employee_benefits->update();
+
         return redirect()->back()->with("success","Benefits saved successfully");
     }
 }
--- resources/views/employee/employee-benefits.blade.php
+++ resources/views/employee/employee-benefits.blade.php
@@ -9,7 +9,7 @@
 @stop
 @section("content")
     @include("employee.category-list")
-    <div class="container mx-auto">
+    <div class="container mx-auto mb-5">
         <a class="btn btn-outline align-right mb-3" href="{{route('display-cart')}}">View Cart</a>
         <h1 class="my-3 text-center"><strong><i>{{$category->name}}</i></strong></h1>
         <h4 class="my-3 text-right">Balance Benefit Amount: {{session("benefit_amount")}}</h4>
@@ -28,29 +28,49 @@
             @endphp
             <div class="row">
                 @foreach($benefits_list as $benefit)
+                    @php
+                        $id = $benefit->id;
+                    @endphp
                     <div class="col-md-4 col-12 text-center">
                         <img class="mb-3" src="{{asset('images/benefits/'.$benefit->image_name)}}"/><br/>
-                        <h4 id="{{'name'.$benefit->id}}">{{$benefit->name}}</h4>
-                        <select id="{{'values'.$benefit->id}}">
+                        <h4 id="{{'name'.$id}}">{{$benefit->name}}</h4>
+                        <select id="{{'values'.$id}}">
                             @foreach($values as $value)
                                 <option value="{{$value}}">{{$value}}</option>
                             @endforeach
                         </select>
-                        <input type="numeric" placeholder="Enter Quantity" id="{{'quantity'.$benefit->id}}" oninput="this.value = this.value.replace(/[^0-9]/g, '').replace(/(\..*)\./g, '$1');" required/>
+                        <input type="numeric" placeholder="Enter Quantity" id="{{'quantity'.$id}}" oninput="this.value = this.value.replace(/[^0-9]/g, '').replace(/(\..*)\./g, '$1');" required/>
                         <br/>
-                        <button class="btn btn-colored mt-4 save-dropdown" id="{{'save'.$benefit->id}}">Save Details</button>
+                        <button class="btn btn-colored mt-4 save-dropdown" id="{{'save'.$id}}">Save Details</button>
                     </div>
                 @endforeach
             </div>
             @elseif($type=="Free Number")
                 @foreach($benefits_list as $benefit)
+                    @php
+                        $id = $benefit->id;
+                    @endphp
                     <div class="col-md-4 text-center">
-                    <img class="mb-3" src="{{asset('images/benefits/'.$benefit->image_name)}}"/><br/>
-                    <h4 id="{{'name'.$benefit->id}}">{{$benefit->name}}</h4>
-                    <input type="numeric" class="mt-2" placeholder="Enter the amount" id="{{'amount'.$benefit->id}}" oninput="this.value = this.value.replace(/[^0-9]/g, '').replace(/(\..*)\./g, '$1');" required/><br/>
-                    <button class="btn btn-colored mt-4 save-amount" id="{{'save'.$benefit->id}}">Save Amount</button>
+                        <img class="mb-3" src="{{asset('images/benefits/'.$benefit->image_name)}}"/><br/>
+                        <h4 id="{{'name'.$id}}">{{$benefit->name}}</h4>
+                        <input type="numeric" class="mt-2" placeholder="Enter the amount" id="{{'amount'.$id}}" oninput="this.value = this.value.replace(/[^0-9]/g, '').replace(/(\..*)\./g, '$1');" required/><br/>
+                        <button class="btn btn-colored mt-4 save-amount" id="{{'save'.$id}}">Save Amount</button>
                     </div>
                 @endforeach
+            @elseif($type=="Text")
+                <div class="row">
+                    @foreach($benefits_list as $benefit)
+                    @php
+                        $id = $benefit->id;
+                    @endphp
+                        <div class="col-md-4 text-center">
+                            <img class="mb-3" src="{{asset('images/benefits/'.$benefit->image_name)}}"/><br/>
+                            <h4 id="{{'name'.$id}}">{{$benefit->name}}</h4>
+                            <textarea style="width: 100%;" rows="5" id="{{'desc'.$id}}"></textarea><br/>
+                            <button class="btn btn-colored mt-4 save save-text" id="{{'save'.$id}}">Save Details</button>
+                        </div>
+                    @endforeach
+                </div>
             @else
 
             @endif
@@ -98,5 +118,25 @@
                 $("#add-to-cart-form").submit();
             }            
         });
+
+        $(".save-text").click(function(e){
+            $length = $(this).attr("id").length;
+            $id=$(this).attr("id").substring(4,$length);
+            $desc = $("#desc"+$id).val();
+            if($desc==""){
+                alert("Description of the gadget required");
+                e.preventDefault();
+            }
+            else{
+                $("#benefit_id").prop("value",$id);
+                $("#name").prop("value",$("#name"+$id).html());
+                $("#quantity").prop("value",0);
+                $("#price").prop("value",0);
+                $("#description").val($desc);
+                $("#add-to-cart-form").attr("action","/save-cart-text");
+                $("#add-to-cart-form").submit();      
+            }            
+        });
+
     </script>
 @stop
